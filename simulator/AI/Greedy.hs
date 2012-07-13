84e3d9efa34379898cb4aa02d6d18c241a2e87e5
Takayuki Muranushi <muranushi@gmail.com>
2012-07-14 02:56:23 +0900

    moge

--- simulator/AI/Greedy.hs
+++ simulator/AI/Greedy.hs
@@ -1,1 +1,65 @@
-module AI.Greedy where
+module Main (main) where
+
+import Control.Applicative
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Monad.Trans.Loop
+import Control.Monad.Trans
+import Data.Lens
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Storable as U
+import qualified Data.Vector.Storable.Mutable as UM
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as GM
+
+import System.IO
+
+import           AI.Common
+import qualified Ans as Ans
+import           LL
+import           Pos
+import           DefaultMain
+
+
+
+main = defaultMain greedySolver
+
+
+atomift :: (MonadIO m) => STM a -> m a
+atomift = liftIO . atomically 
+
+greedySolver :: (MonadIO m) => Solver m
+greedySolver = safetynet $ do
+  bd <- access llBoardL  
+  guide <- liftIO $ V.unsafeThaw =<< V.mapM UM.clone =<< V.unsafeFreeze bd  
+  let h = GM.length bd
+  w <- liftIO $ GM.length <$> GM.read bd 0
+  
+  probes <- atomift $ newTQueue
+  foreach [0..h-1] $ \y -> do
+    foreach [0..w-1] $ \x -> do
+      let r = Pos x y
+      a <- readPos bd r
+      when (a == '\\' || a == 'O') $ do
+        writePos guide r 'W'        
+        atomift $ writeTQueue probes r
+        continue
+      when (a == '.' || a == ' ' || a == 'R') $ do      
+        writePos guide r ' '
+        continue
+      writePos guide r 'A'        
+  while (atomift (not <$> isEmptyTQueue probes)) $ do
+    r <- atomift $ readTQueue probes
+    foreach directions $ \ (symbol, dr) -> do
+      let nr = r - dr
+      nc <- readPos guide nr
+      when (nc==' ') $ do
+        writePos guide nr symbol
+        atomift $ writeTQueue probes nr
+  withBackup $ do
+    llBoardL ~= guide
+    --showBoard
+  roboPos <- access llPosL
+  cmd <- readPos guide roboPos
+  return $ Ans.Cont cmd
--- simulator/Ans.hs
+++ simulator/Ans.hs
@@ -1,3 +1,3 @@
 module Ans where
 
-data Ans = Cont Char | Undo | End
+data Ans = Cont Char | Undo | End deriving (Eq, Show)

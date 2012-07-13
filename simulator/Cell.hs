353018acf16a01d36d96414badee6a80231960d8
片岡 俊基 <tos@knit.local>
2012-07-13 23:28:14 +0900

    Cell.hs

---
+++ simulator/Cell.hs
@@ -0,0 +1,31 @@
+module Cell
+  ( Cell (..)
+  , showCell, showCells
+  , readCell
+  ) where
+
+data Cell = Robot | Wall | Rock | Lambda | ClosedLift | OpenLift | Earth | Empty
+  deriving (Show, Read)
+
+showCell Robot = "R"
+showCell Wall = "#"
+showCell Rock = "*"
+showCell Lambda = "\\"
+showCell ClosedLift = "L"
+showCell OpenLift = "O"
+showCell Earth = "."
+showCell Empty = " "
+
+showCells = concatMap showCell
+
+readCell 'R' = Robot
+readCell '#' = Wall
+readCell '*' = Rock
+readCell '\\'= Lambda
+readCell 'L' = ClosedLift
+readCell 'O' = OpenLift
+readCell '.' = Earth
+readCell ' ' = Empty
+-- readCell _ = 
+
+readCells = map readCell

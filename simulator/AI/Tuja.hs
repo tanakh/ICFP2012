7629b3f035c24ec34a816dc05f83c46801c986ad
Takayuki Muranushi <muranushi@gmail.com>
2012-07-14 21:38:14 +0900

    tekit

--- simulator/AI/Tuja.hs
+++ simulator/AI/Tuja.hs
@@ -29,32 +29,33 @@ import Text.Printf
 
 import           AI.Common
 import           AI.GorinNoSho
-import qualified Ans
+import qualified Ans as Ans
 import           DefaultMain
+import qualified Flood
 import           LL
-import qualified Option
+import qualified Option as Option
 import           Pos
 
-newtype Wave =
-  Wave
+newtype Wave = 
+  Wave  
   [(Double, -- amplitude
     Double, -- freq
     Double -- initial phase
     )] deriving (Eq, Show, Read)
 
 toAmp :: Double -> Wave -> Double
-toAmp t (Wave xs) =
+toAmp t (Wave xs) = 
   sum $ map (\(a,f,p0) -> a*cos(f*t+p0)) xs
 
 toAmp2 :: Double -> Wave -> Dpos
-toAmp2 t (Wave xs) =
+toAmp2 t (Wave xs) = 
   sum $ map (\(a,f,p0) -> Pos (a*cos(f*t+p0)) (a*sin(f*t+p0)) ) xs
 
 
-data Config =
-  Config
+data Config = 
+  Config 
   {
-    searchAtom ::
+    searchAtom :: 
        [(Wave, -- weight
          String, -- source terrains
          String  -- passable terrains
@@ -66,7 +67,7 @@ data Config =
   } deriving (Eq, Show, Read)
 
 randomConfig :: IO Config
-randomConfig =
+randomConfig = 
   Config <$> randomSearchAtom <*> randomMany 5 randomWindAtom1
 
 
@@ -75,11 +76,11 @@ choose xs = do
   i <- randomRIO (0,length xs-1)
   return $ xs !! i
 
-normalSearchAtom =
-    (Wave [(2.302,0,0)], "\\O", " .")
+normalSearchAtom = 
+    (Wave [(2.302, 0, 0)], "\\O", " .")
 
 randomSearchAtom :: IO [(Wave, String, String)]
-randomSearchAtom =
+randomSearchAtom = 
   (normalSearchAtom :) <$> randomMany 3 randomSearchAtom1
 
 randomSearchAtom1 = do
@@ -114,10 +115,14 @@ randomMany num m = do
                 else return []
   return $ x:xs
 
-data Resource =
+
+
+data Tejun = Tejun Int Result String
+
+data Resource = 
   Resource {
     dijkstraMaps :: IORef (Map.Map String (Field Double)),
-    submitter :: (Result, String) -> IO ()
+    submitter :: Tejun -> IO ()
            }
 initResource :: IO Resource
 initResource = Resource <$> newIORef (Map.empty) <*> pure (\_ -> return ())
@@ -128,7 +133,7 @@ getCurrentTime = read . show <$> epochTime
 main :: IO ()
 main = do
   startTime <- getCurrentTime
-  opt <- Option.parseIO
+  opt <- Option.parseIO  
   txt <- case Option.input opt of
     Option.Stdin -> getContents
     Option.InputFile fn -> readFile fn
@@ -139,18 +144,18 @@ main = do
       fld = Flood.readFlood $ drop 1 txtM
   case Option.mode opt of
     Option.Ninja -> ninjaMain opt startTime  fld bd
-    Option.Survey -> do
+    Option.Survey -> do  
       res <- initResource
-      config <- randomConfig
+      config <- randomConfig  
 
       res <- runLLT fld bd $ simpleSolver res config
       let fnInput = case Option.input opt of
             Option.InputFile fp -> fp
-            Option.Stdin -> "STDIN"
+            Option.Stdin -> "STDIN"  
           fnRec = (printf "record/%s-%d-%s.txt"
                 (dropExtension $ takeFileName fnInput)
                 (scoreResult res)
-                (take 6 $ show $ md5 $ L.pack $ show config))
+                (take 6 $ show $ md5 $ L.pack $ show config)) 
       hPutStrLn stderr fnRec
       liftIO $ system "mkdir -p record/"
       liftIO $ writeFile fnRec $
@@ -162,7 +167,7 @@ main = do
 
 ninjaMain :: Option.Option -> Int -> Flood.Flood -> [String] -> IO ()
 ninjaMain opt startTime fld bd = do
-  submitQ <- newTQueueIO
+  submitQ <- newTQueueIO 
   replicateM_ 100 $ do
     res0 <- initResource
     let res = res0 { submitter = \x -> atomically $ writeTQueue submitQ x}
@@ -180,25 +185,19 @@ waitOhagi opt startTime = do
 isEffectiveMove :: (MonadIO m) => Char -> LLT m Bool
 isEffectiveMove hand = return True
 
-simpleSolver :: IORef (Maybe (Field Double)) -> Config -> Solver IO
-simpleSolver smellRef config = safetynet $ do
-  -- setup initial valmap, dijkstra field
+simpleSolver :: (Functor m, MonadIO m) 
+                => Resource
+                -> Config 
+                -> LLT m Result
+simpleSolver resource config = do
   bd <- access llBoardL
-  validHands <- filterM isEffectiveMove "LRUD"
-  -- dono te ga tsuyoinoka; watashi kininarimasu!
-  hyokaRef <- liftIO $ newIORef $
+  validHands <- filterM isEffectiveMove "LRUD"  
+  -- dono te ga tsuyoinoka; watashi kininarimasu! 
+  hyokaRef <- liftIO $ newIORef $ 
                 Map.fromList [(hand,0::Double) | hand <- validHands]
   let addHyoka hand val =
         liftIO $ modifyIORef hyokaRef (Map.update (Just . (val+)) hand)
   roboPos <- access llPosL
-
-  maybeSmell <- liftIO $ readIORef smellRef
-  smell <- case maybeSmell of
-    Just a -> return a
-    Nothing -> do
-      b <- newF (1::Double)
-      liftIO $ writeIORef smellRef $ Just b
-      return b
 
   step <- access llStepL
   let time :: Double
@@ -207,24 +206,49 @@ simpleSolver smellRef config = safetynet $ do
 
   -- treat each wind
   forM_ (windAtom config) $ \ (wave, windWave) -> do
-    forM_ validHands $ \hand -> do
+    forM_ validHands $ \hand -> do      
       let vec = fromIntegral <$> hand2pos hand
           windVec = toAmp2 time windWave
-          val = (exp . toAmp time) wave * (vec `innerProd` windVec)
+          val = (sinh . toAmp time) wave * (vec `innerProd` windVec)    
       addHyoka hand val
   -- treat each search
   forM_ (searchAtom config) $ \ (wave, srcStr, passStr) -> do
+    dm <- liftIO $ readIORef $ dijkstraMaps resource
+    let maybeSmell = Map.lookup srcStr dm
+    smell <- case maybeSmell of
+      Just a -> return a
+      Nothing -> do
+        b <- newF 0
+        liftIO $ modifyIORef (dijkstraMaps resource) $ 
+          Map.insert srcStr b
+        return b
     dijkstra smell srcStr passStr 1
     updateF smell (\x -> if isPassable x then 1/x else 0)
-    -- showF (wideShow 5) smell --debug
     forM_ validHands $ \hand -> do
       smellAt <- unsafeReadF smell (roboPos + hand2pos hand)
-      addHyoka hand $ (exp . toAmp time) wave * smellAt
+      addHyoka hand $ (sinh . toAmp time) wave * smellAt
 
   hyokaMap <- liftIO $ readIORef hyokaRef
-  if step > 1000
-     then return $ Ans.Cont 'A'
-     else return $ Ans.Cont $
-       snd $ last $ sort $
-       map (\(hand,val) -> (val,hand)) $
-       Map.toList $ hyokaMap
+  let ansHand = 
+        if step > 1000
+        then 'A'
+        else 
+          snd $ last $ sort $  
+          map (\(hand,val) -> (val,hand)) $
+          Map.toList $ hyokaMap
+  
+  lam0 <- access llLambdasL
+  simulateStep ansHand
+  lam1 <- access llLambdasL
+  when (lam1 /= lam0) $ do
+    sco <- abortScore
+    rep <- getReplay
+    liftIO $ submitter resource $ Tejun sco Abort rep
+  maybeS <- 
+  case res of
+    Cont -> simpleSolver resource config
+    _        -> do
+      rep <- reverse <$> access llReplayL
+      score 
+      liftIO $ submitter resource $ Tejun sco res rep
+      return res

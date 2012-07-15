a7c1e67876796f4c3c252aefb1e2fa48e76b843b
Takahiro HIMURA <taka@himura.jp>
2012-07-16 04:58:36 +0900

    oops

--- matchlist/server.hs
+++ matchlist/server.hs
@@ -71,22 +71,17 @@ getResults results = do
 renderTable :: [Maybe SolvedResult] -> IO [Text]
 renderTable results = forM results $ \r -> do
   case r of
-    Just v -> mustache "./views/td.mustache" v
+    Just v | isJust (sExitCode v) -> mustache "./views/td.mustache" v
+           | otherwise -> mustache "./views/td.lte.mustache" v
     Nothing -> return "<td>N/A</td>"
-
-main :: IO ()
-main = do
-  renderResult >>= TL.putStrLn
-  -- serv
 
 renderResult :: IO Text
 renderResult = do
   results <- getSolvedResultList >>= getResults
   mustache "./views/home.mustache" results
 
--- does not works. i dont know why
-serv :: IO ()
-serv = scotty 3000 $ do
+main :: IO ()
+main = scotty 3000 $ do
     middleware $ staticPolicy $ addBase "results"
 
     get "/" $ do

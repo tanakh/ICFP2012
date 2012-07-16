6e711b2b3c3871edbdac456e188edbbd280e75d3
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-07-16 17:29:59 +0900

    ほげー

--- simulator/AI/BF.hs
+++ simulator/AI/BF.hs
@@ -25,9 +25,10 @@ import AI.GorinNoSho
 import AI.Oracle
 import qualified Option
 import AI.Cooking(choose)
+import Pos
 
 minf :: Int
-minf = -10^(9::Int)
+minf = -10^(8::Int)
 
 moves :: [Char]
 moves = "LRUDWSA"
@@ -41,6 +42,8 @@ data CacheEntry
     { ceStep      :: {-# UNPACK #-} !Int
     , ceWaterStep :: {-# UNPACK #-} !Int
     , ceRazors    :: {-# UNPACK #-} !Int
+    , cePenalty   :: {-# UNPACK #-} !Int
+    , ceScore     :: {-# UNPACK #-} !Int
     }
 
 isWorseThan :: CacheEntry -> CacheEntry -> Bool
@@ -48,17 +51,36 @@ a `isWorseThan` b =
   ceStep a >= ceStep b
   && ceWaterStep a >= ceWaterStep b
   && ceRazors a <= ceRazors b
+  && (ceScore a, 0) <= (ceScore b, 0)
+  --  && (ceScore a, - cePenalty a) <= (ceScore b, - cePenalty b)
 
-addCacheEntry :: IORef (HM.HashMap Word64 [CacheEntry]) -> LLState -> IO Bool
-addCacheEntry hmr st = do
-  hm <- readIORef hmr
-  let ce = CacheEntry (llStep st) (llWaterStep st) (llRazors st)
+addCacheEntry :: IORef (HM.HashMap Word64 [CacheEntry]) -> LL Int -> LL Int
+addCacheEntry hmr m = do
+  st <- get
+
+  hm <- liftIO $ readIORef hmr
+  let ce = CacheEntry
+             { ceStep = llStep st
+             , ceWaterStep = llWaterStep st
+             , ceRazors = llRazors st
+             , cePenalty = llPenalty st
+             , ceScore = llLambdas st
+             }
   let cm = case HM.lookup (llHash st) hm of
-        Just cfs -> not $ any (ce `isWorseThan`) cfs
-        _ -> True
-  when cm $ do
-    modifyIORef hmr $ HM.insertWith (++) (llHash st) [ce]
-  return cm
+        Just cfs
+          | not $ any (ce `isWorseThan`) cfs -> Nothing
+          | otherwise -> Just $ maximum $ map ceScore cfs
+        _ -> Nothing
+
+  case cm of
+    Nothing -> do
+      liftIO $ modifyIORef hmr $ HM.insertWith (++) (llHash st) [ce]
+      sc <- m
+      --liftIO $ modifyIORef hmr $ HM.insertWith (++) (llHash st)
+      --  [ce { ceScore = sc }]
+      return sc
+    Just _ ->
+      return minf
 
 safetyCheck :: HS.HashSet Word64 -> Int ->  Char -> LL Bool
 safetyCheck hist fuel hand
@@ -68,6 +90,27 @@ safetyCheck hist fuel hand
                        return $ res /= Dead && not (HS.member h hist)
   | otherwise = withStep hand $
                      or <$> (forM "LRUD" $ safetyCheck hist (fuel-1))
+
+adjR =
+  [ Pos 1 0
+  , Pos 0 1
+  , Pos (-1) 0
+  ]
+
+simplify :: LL ()
+simplify = do
+  bd <- access llBoardL
+  forPos $ \p -> do
+    cell <- liftIO $ readPos bd p
+    liftIO $ when (cell == '.') $ do
+      keep <- forM adjR $ \((+p) -> n) -> do
+        nell <- readPos bd n
+        return $ isRock nell || nell == 'W'
+      when (not $ or keep) $ do
+        writePos bd p ' '
+  liftIO $ putStrLn "simplify to:"
+  showBoard
+
 main :: IO ()
 main = do
   opt <- Option.parseIO
@@ -82,19 +125,22 @@ main = do
     step <- access llStepL
     (liftIO . Oracle.submit oracle) =<< getAbortTejun
 
-    bfDepth <- liftIO $ Oracle.ask oracle "bfDepth" $ return 10
+    bfDepth <- liftIO $ Oracle.ask oracle "bfDepth" $ return 20
     hmr <- liftIO $ newIORef HM.empty
     hashNow <- access llHashL
-    
+
     kabutta <- liftIO $ do
            modifyIORef hashLogRef (hashNow:)
            xs <- readIORef hashLogRef
-           return $ (length (take 3 xs) >= 3) && ((1==) $length $ nub $take 3 xs)
+           return $
+             (length (take 3 xs) >= 3)
+             && ((1==) $length $ nub $take 3 xs)
     liftIO $ modifyIORef historyRef $ HS.insert hashNow
     history <- liftIO $ readIORef historyRef
 
 
     (mov, sc) <- withBackup $ do
+      simplify
       rs <- forM moves $ \mov -> do
         cur <- get
         b <- prePruning cur mov
@@ -107,6 +153,9 @@ main = do
               then (mov, ) <$> eval undefined hmr 0 bfDepth
               else return (mov, minf)
           else return (mov, minf)
+      liftIO $ print rs
+      liftIO $ print . HM.size =<< readIORef hmr
+      liftIO $ print . sum . map length . map snd . HM.toList =<< readIORef hmr
       return $ maximumBy (comparing snd) rs
 
     greedyDepth <- liftIO $ Oracle.ask oracle "greedyDepth" $ return 4
@@ -156,11 +205,13 @@ pruning cur move next
   | otherwise =
     return True
 
+scoreOfs = 10^5
+
 staticScore :: Field Int -> LL Int
 staticScore _ = do
   LLState {..} <- get
   abt <- abortScore
-  return abt
+  return $ (abt * scoreOfs) - llPenalty
   {-
   aScore <- abortScore
   pos <- access llPosL
@@ -173,18 +224,14 @@ eval :: Field Int -> Cache -> Int -> Int -> LL Int
 eval valueField cache !curBest !fuel = do
   mb <- score
   case mb of
-    Just sc -> return sc
+    Just sc -> return $ sc * scoreOfs
     _ | fuel <= 0 -> staticScore valueField
     _ -> do
-      st <- get
-      ok <- liftIO $ addCacheEntry cache st
-      if ok
-        then do
+      addCacheEntry cache $ do
         best cache moves $ \mov -> do
           -- liftIO $ putStrLn $ "fuel: " ++ show fuel ++ ", mov: " ++ [mov]
           -- showStatus
           eval valueField cache curBest (fuel - 1)
-        else return minf
 
 best :: Cache -> [Char] -> (Char -> LL Int) -> LL Int
 best cache ls m = do

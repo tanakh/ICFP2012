3a067ab977278ddac595ef4861a2f2e0a3a824ea
Takahiro HIMURA <taka@himura.jp>
2012-07-16 18:42:51 +0900

    fixed bug

--- matchlist/run_ais.hs
+++ matchlist/run_ais.hs
@@ -9,9 +9,9 @@ import qualified System.FilePath as FilePath
 import qualified System.IO as IO
 
 import qualified GHC.IO.Handle as H
-
+import qualified System.Posix.Signals as Signal
 import qualified System.Posix.Process as P
-import System.Posix.Types (ProcessID (..))
+import System.Posix.Types (ProcessID)
 
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy.Char8 as BL8
@@ -25,7 +25,6 @@ import AIResult
 data Option =
   Option { maplist :: FilePath
          , ailist  :: FilePath
-         , destdir :: Maybe FilePath
          , waittime :: Int
          , waitmore :: Int
          }
@@ -50,12 +49,6 @@ parse = Option
           & value "ailist"
           & help "ailist" )
         <*> strOption (
-          long "dest"
-          & short 'd'
-          & transform Just
-          & value Nothing
-          & help "dest" )
-        <*> strOption (
           long "wait"
           & short 'w'
           & transform read
@@ -70,12 +63,11 @@ parse = Option
 
 updateAI :: FilePath
          -> FilePath
-         -> Maybe FilePath
          -> FilePath
          -> Int
          -> Int
          -> IO ()
-updateAI aipath mappath dest basename wait waitm = do
+updateAI aipath mappath basename wait waitm = do
   exist <- FS.isFile rfpath
   if exist
     then do
@@ -87,7 +79,7 @@ updateAI aipath mappath dest basename wait waitm = do
   where
     run = do
       putStrLn basename
-      result <- runAI aipath mappath dest basename wait waitm
+      result <- runAI aipath mappath basename wait waitm
       BL8.writeFile resultFile $ A.encode $ A.toJSON result
     resultFile = basename ++ ".result"
     rfpath = FP.decodeString resultFile
@@ -103,22 +95,21 @@ createProcess
   :: FilePath -> [String] -> H.Handle -> H.Handle
   -> IO ProcessID
 createProcess cmd args stdo stde = P.forkProcess $ do
-  stdout_dup <- H.hDuplicate IO.stdout
+  void $ H.hDuplicate IO.stdout
   H.hDuplicateTo stdo IO.stdout
   H.hClose stdo
-  stderr_dup <- H.hDuplicate IO.stderr
+  void $ H.hDuplicate IO.stderr
   H.hDuplicateTo stde IO.stderr
   H.hClose stde
   P.executeFile cmd True args Nothing
 
 runAI :: FilePath
       -> FilePath
-      -> Maybe FilePath
       -> FilePath
       -> Int
       -> Int
       -> IO SolvedResult
-runAI aipath mappath dest basename wait waitm = do
+runAI aipath mappath basename wait waitm = do
   let errfn = basename ++ ".err.txt"
       outfn = basename ++ ".out.txt"
       aname = FilePath.takeBaseName aipath
@@ -127,40 +118,47 @@ runAI aipath mappath dest basename wait waitm = do
       mapspec = MapSpec mname mappath
 
   mapdata <- IO.readFile mappath
-  IO.withFile outfn IO.WriteMode $ \hout ->
+  result <- newEmptyMVar
+  (start, _, _, wth) <- IO.withFile outfn IO.WriteMode $ \hout ->
     IO.withFile errfn IO.WriteMode $ \herr -> do
       start <- getCurrentTime
-      result <- newEmptyMVar
 
       pid <- createProcess aipath ["-i", mappath, "-t", show wait, "-v"] hout herr
       th <- forkIO $ do
-        ret <- P.getProcessStatus Trule False pid
-        
-        putMVar result . Just
+        mstat <- P.getProcessStatus True False pid
+        let st = case mstat of
+              Just (P.Exited ec) -> Just ec
+              _ -> Nothing
+        void $ tryPutMVar result st
+
       wth <- forkIO $ do
         putStrLn $ "waiting for " ++ basename ++ " " ++ show wait
         threadDelay (wait * 1000 * 1000)
-        putStrLn $ "send SIGINT: " ++ basename ++ " waiting: " ++ show waitm
-        P.terminateProcess pid
+        putStrLn $ ("will send SIGINT to " ++ basename ++ ", and waiting " ++ show waitm)
+        Signal.signalProcess Signal.sigINT pid
+        threadDelay (waitm * 1000 * 1000)
+        putStrLn $ "send SIGTERM: " ++ basename
+        Signal.signalProcess Signal.sigTERM pid
         killThread th
-        putStr $ "terminated " ++ basename
+        putStrLn $ "terminated " ++ basename
         void $ tryPutMVar result Nothing
 
-      ret <- takeMVar result
-      elapsed <- (`diffUTCTime` start) <$> getCurrentTime
-      let es = fromRational . toRational $ elapsed
-      killThread wth
+      return (start, pid, th, wth)
+  ret <- takeMVar result
+  elapsed <- (`diffUTCTime` start) <$> getCurrentTime
+  let es = fromRational . toRational $ elapsed
+  killThread wth
 
-      ans <- getAnswer outfn
-      score <- calcScore mapdata ans
+  ans <- getAnswer outfn
+  score <- calcScore mapdata ans
 
-      return $ SolvedResult { sExitCode = fromExitCode <$> ret
-                            , sMapSpec = mapspec
-                            , sAISpec = aispec
-                            , sBaseName = basename
-                            , sScore = score
-                            , sTimeElapsed = es
-                            }
+  return $ SolvedResult { sExitCode = fromExitCode <$> ret
+                        , sMapSpec = mapspec
+                        , sAISpec = aispec
+                        , sBaseName = basename
+                        , sScore = score
+                        , sTimeElapsed = es
+                        }
 
   where
     fromExitCode ExitSuccess = 0
@@ -176,4 +174,4 @@ main = do
     forM_ ais $ \ai -> do
       let ainame = FilePath.takeBaseName ai
           basename = ainame ++ "_" ++ mapname
-      updateAI ai m (destdir opt) basename (waittime opt) (waitmore opt)
+      updateAI ai m basename (waittime opt) (waitmore opt)
